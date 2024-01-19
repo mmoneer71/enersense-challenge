166007cf030a0717c55d724cb306dfe1e792e866
Moneer <34192678+mmoneer71@users.noreply.github.com>
2024-01-19 15:42:37 +0200

    Update settings.py

--- api/utils/settings.py
+++ api/utils/settings.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
-
+# ditch this and do it the data-api way aka pydantic settings
 MONGO_DB_USERNAME = os.getenv("MONGO_DB_USERNAME", "enersense-assignment")
 MONGO_DB_PASSWORD = os.getenv("MONGO_DB_PASSWORD", "")
