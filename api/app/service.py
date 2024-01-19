80858722fd2b1b0c1f7af3fa19f47942168d4519
Moneer <34192678+mmoneer71@users.noreply.github.com>
2024-01-19 15:31:12 +0200

    Update service.py

--- api/app/service.py
+++ api/app/service.py
@@ -3,7 +3,9 @@ from utils.logger import get_logger
 
 logger = get_logger("api_service")
 
-
+# if you don't know, favor async
+# if you know what you're doing don't use async
+# if you are sure what you are doing, feel free to choose yourself
 async def get_all_readings(conn: AsyncIOMotorClient):
     logger.info("Retreiving all readings from db")
     readings = await conn["charger_payload"].find().to_list(1000)
